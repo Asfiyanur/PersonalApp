886617933f93ef1f7400ee030819714260cd7d7e
Asfiyanur <nurasfiya.13@gmail.com>
2023-01-20 14:11:08 +0300

    feat : Department View

--- personal/views.py
+++ personal/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .serializers import DepartmentSerializers
+from rest_framework import generics
+from .models import Department,Personal
 
 # Create your views here.
+
+
+class DepartmentView(generics.ListCreateAPIView):
+    serializer_class = DepartmentSerializers
+    queryset = Department.objects.all()
